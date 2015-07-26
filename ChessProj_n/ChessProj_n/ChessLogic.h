f73717a5f089504d4ef66a1ab692157f1eca16e3
along245 <along245@gmail.com>
2015-07-26 15:35:31 +0300

    aja

--- ChessProj_n/ChessProj_n/ChessLogic.h
+++ ChessProj_n/ChessProj_n/ChessLogic.h
@@ -1,56 +1,48 @@
-#ifndef DRAUGHTS_
-#define DRAUGHTS_
+#ifndef CHESS_
+#define CHESS_
 
-#include <stdio.h>
+#include<stdio.h>
+
+#define WHITE_P 'm'
+#define WHITE_B 'b'
+#define WHITE_N 'n'
+#define WHITE_R 'r'
+#define WHITE_Q 'q'
+#define WHITE_K 'k'
+
+#define BLACK_P 'M'
+#define BLACK_B 'B'
+#define BLACK_N 'N'
+#define BLACK_R 'R'
+#define BLACK_Q 'Q'
+#define BLACK_K 'K'
 
 
-#define WHITE_KING 'k'
-#define WHITE_ROOK 'r'
-#define WHITE_PAWN 'p'
-#define WHITE_BISHOP 'b'
-#define WHITE_QUEEN 'q'
-#define WHITE_KNIGHT 'n'
-
-#define BLACK_KING 'K'
-#define BLACK_ROOK 'R'
-#define BLACK_PAWN 'P'
-#define BLACK_BISHOP 'B'
-#define BLACK_QUEEN 'Q'
-#define BLACK_KNIGHT 'N'
-
-#define WHITE 1
-#define BLACK -1
 #define EMPTY ' '
-#define WHITE_T WHITE//white turn
-#define BLACK_T BLACK//black turn
-#define WHITE_COL 'w'
-#define BLACK_COL 'b'
 
+#define BOARD_SIZE 8
 
-
-
-
-#define BOARD_SIZE 10
-
-typedef char** board_t;
-#define WELCOME_TO_DRAUGHTS "Welcome to Draughts!\n"
+// board initialization
 #define ENTER_SETTINGS "Enter game settings:\n" 
-#define WRONG_MINIMAX_DEPTH "Wrong value for minimax depth. The value should be between 1 to 6\n"
+#define WRONG_GAME_MODE "Wrong game mode\n"
+#define TWO_PLAYERS_GAME_MODE "Running game in 2 players mode\n"
+#define PLAYER_VS_AI_GAME_MODE "Running game in player vs. AI mode\n"
+#define WRONG_MINIMAX_DEPTH "Wrong value for minimax depth. The value should be between 1 to 4\n"
+#define WRONG_FILE_NAME "Wrong file name\n"
 #define WRONG_POSITION "Invalid position on the board\n"
-#define NO_DICS "The specified position does not contain your piece\n"
-#define ILLEGAL_COMMAND "Illegal command, please try again\n"
-#define ILLEGAL_MOVE "Illegal move\n"
+#define NO_PIECE "Setting this piece creates an invalid board\n"  
 #define WROND_BOARD_INITIALIZATION "Wrong board initialization\n"
 
-#define ENTER_YOUR_MOVE "Enter your move:\n" 
-#define perror_message(func_name) (fprintf(stderr, "Error: standard function %s has failed\n", func_name))
+#define ILLEGAL_COMMAND "Illegal command, please try again\n"
+#define ILLEGAL_MOVE "Illegal move\n"
+
+#define WRONG_ROOK_POSITION "Wrong position for a rook\n" 
+#define ILLEGAL_CALTLING_MOVE "Illegal castling move\n"  
+
+#define TIE "The game ends in a tie\n"
+
+#define perror_message(func_name) (perror("Error: standard function %s has failed", func_name));
 #define print_message(message) (printf("%s", message));
 
 
-
-void print_board(char board[BOARD_SIZE][BOARD_SIZE]);
-void init_board(char board[BOARD_SIZE][BOARD_SIZE]);
-void clear(char board[BOARD_SIZE][BOARD_SIZE]);
-int calcPos(char i); // given a char representing a column, return its required int value in the matrice
-int evaluate(void* state);
-#endif  
+#endif CHESS_
